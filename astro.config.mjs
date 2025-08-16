673006be27e8d30f8d1cc0d2d6d63a98f3626891
xlqmu <3212929002@qq.com>
2025-08-16 11:35:21 +0800

    fix: Corrected errors in icons and personal information

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://your-domain.com',
+	site: 'https://xlqmu.com',
 	integrations: [mdx(), sitemap(), tailwind()],
 });
